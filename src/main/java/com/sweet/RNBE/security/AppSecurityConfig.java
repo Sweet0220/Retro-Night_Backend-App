28aec46f4020937f0fb9617f2260377e7a39717b
Jeff0220 <MNealcos@gmail.com>
2022-04-04 17:27:27 +0300

    Switched default login page.

---
+++ src/main/java/com/sweet/RNBE/security/AppSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sweet.RNBE.security;
+
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/js/**", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll();
+    }
+}
